a248433d2b95a76a9a8ad9edebedefd3a77b9cb7
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-19 13:41:53 -0800

    commit tree 174d65452130cad1358ee16bd3612edd80472319

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -2,9 +2,49 @@
 import React, { Component } from 'react';
 
 export default class YoutubeDebugger extends Component{
+    constructor(){
+        super();
+        this.state =
+            {
+                errors: [],
+                user: null,
+                settings: {
+                  bitrate: 8,
+                  video: {
+                    resolution: '1080p'
+                  }
+                }
+              }
+    }
+
+    handleBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    handleResolution = () => {
+        let x = this.state.settings.video;
+        x.resolution = '720p';
+        console.log(x.resolution)
+        // this.setState({
+        //     settings: {
+        //         ...this.state.settings.video,
+        //         resolution: '720p'
+        //     }
+        // })
+        return x;
+    }
+
     render(){
         return(
-            <button></button>
+            <div>
+                <button className='bitrate' onClick={this.handleBitrate}>{this.state.settings.bitrate}</button>
+                <button className='resolution' onClick={this.handleResolution}></button>
+            </div>
         )
     }
 }
