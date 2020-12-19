e25daaddb6b89ce60f018bc947e56f6de971b822
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-19 12:41:07 -0800

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,29 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component{
+    constructor(){
+        super();
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () => {
+        let newCount = this.state.timesClicked + 1;
+        this.setState({
+            timesClicked: newCount
+        })
+        // this.setState(previousState => {
+        //     return{
+        //         timesClicked: previousState.count + 1
+        //     }
+        // })
+    }
+
+    render(){
+        return(
+            <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,10 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YoutubeDebugger extends Component{
+    render(){
+        return(
+            <button></button>
+        )
+    }
+}
